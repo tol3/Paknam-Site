2f2f3ced22535bb3d1b372cb4d445750976cd9d6
o0KuReNai0o <demon.nae@gmail.com>
2014-09-14 16:44:14 +0700

    Changed nav bar and footer

---
+++ app/assets/javascripts/scripts.js
@@ -0,0 +1,8 @@
+$(function(){
+	$('.animated > li').hover(function(){
+		$(this).find('div[class^="container-"]').stop().slideDown('fast');
+	},
+	function(){
+		$(this).find('div[class^="container-"]').stop().slideUp('slow');
+	});
+});
