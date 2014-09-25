530c89b7ddb99a0e0a587996ec65316959e19a08
tol3 <tol3@me.com>
2014-09-25 12:42:41 +0700

    Add puma config & block assets

---
+++ config/puma.rb
@@ -0,0 +1,24 @@
+workers Integer(ENV['PUMA_WORKERS'] || 5)
+threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 5)
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 5555
+environment ENV['RACK_ENV'] || 'production'
+
+on_worker_boot do
+  # worker specific setup
+  puts 'Worker boot...'
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
+
+
+tag 'Paknam'
+
+on_restart do
+	puts 'On restart...'
+end
+
