71412eaa543bbedb05e6b9e2a3aa10058e1e3613
o0KuReNai0o <demon.nae@gmail.com>
2014-09-28 21:47:36 +0700

    Add news and back icon

--- db/migrate/20140925194900_changed_managers_deleted_parent_id.rb
+++
@@ -1,9 +0,0 @@
-class ChangedManagersDeletedParentId < ActiveRecord::Migration
-  def self.up
-    remove_column :managers, :parent_id
-  end
-
-  def self.down
-    add_column :managers, :parent_id, :integer, :limit=>4, :default=>nil
-  end
-end
